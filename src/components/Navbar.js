dfa7c013a202bec65d7708fe3baab8d097d7ca56
Mahveen <98329413+Mahveen96@users.noreply.github.com>
2023-03-20 13:42:40 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <header className="header">
-      <img src="../images/logo.png" alt="" />
+      <img src="./images/logo.png" alt="" />
       <h2>Meme Generator</h2>
       <h4>React Project</h4>
     </header>
